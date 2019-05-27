d14cc059f93ce11ded52ad0f6be77e442094f591
Mo7gameryt <39068442+Mo7gameryt@users.noreply.github.com>
2019-05-27 15:46:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '!#'
 
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`Starting...`,"http://twitch.tv/S-F")
